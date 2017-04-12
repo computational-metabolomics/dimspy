e3d3fc078f76741c069b59e8ea74fb3e0ddb2776
RJMW <r.j.weber@bham.ac.uk>
2017-04-12 15:51:39 +0100

    Update package structure

--- dimspy/__init__.py
+++ dimspy/__init__.py
@@ -1,5 +1,5 @@
 
-__author__ = 'Ralf Weber, Albert Zhou'
-__credits__ = 'Ralf Weber, Albert Zhou'
+__author__ = 'Ralf Weber (r.j.weber@bham.ac.uk), Albert Zhou (j.zhou.3@bham.ac.uk)'
+__credits__ = 'Ralf Weber (r.j.weber@bham.ac.uk), Albert Zhou (j.zhou.3@bham.ac.uk)'
 __version__ = '0.1.0'
 __license__ = 'GPLv3'
