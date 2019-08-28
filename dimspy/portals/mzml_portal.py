2bd304f439e96d2e1cdeba48fdb0a0bce16594c6
Ralf Weber <RJMW@users.noreply.github.com>
2019-08-28 22:41:39 +0100

    Update and fix mzml portal (close handles) (#49)

--- dimspy/portals/mzml_portal.py
+++ dimspy/portals/mzml_portal.py
@@ -13,50 +13,39 @@
 import os
 import collections
 import numpy as np
-import zipfile
-from copy import deepcopy
+import pymzml
 from dimspy.models.peaklist import PeakList
 from dimspy.experiment import mz_range_from_header
 
-import warnings
-warnings.simplefilter("ignore", category=ResourceWarning)
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", category=DeprecationWarning)
-    import pymzml
 
 class Mzml:
     def __init__(self, filename="", preload=True):
         self.filename = filename
-        self._preload = preload
-        self._cache = None
 
-    def run(self):
-        # if self._cache is not None: return self._cache
+        if not os.path.isfile(self.filename):
+            raise IOError("{} does not exist".format(self.filename))
 
         if not self.filename.lower().endswith(".mzml") and not self.filename.lower().endswith(".mzml.gz"):
             raise IOError('Incorrect file format for mzML parser')
-        
-        if self.filename.lower().endswith(".mzml") or self.filename.lower().endswith(".mzml.gz"):
-            if not os.path.isfile(self.filename):
-                raise IOError("{} does not exist".format(self.filename))
-            dat = pymzml.run.Reader(self.filename)
-            # if self._preload: dat = self._cache = tuple(map(deepcopy, dat))
-            return dat
-        else:
-            return None
+
+        self.run = pymzml.run.Reader(self.filename)
 
     def headers(self, n=None):
         h_sids = collections.OrderedDict()
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             if 'MS:1000512' in scan:
                 h_sids.setdefault(scan['MS:1000512'], []).append(scan['id'])
+        run.info["file_object"].close()
         return h_sids
 
     def scan_ids(self):
         h_sids = collections.OrderedDict()
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             if 'MS:1000512' in scan:
                 h_sids[scan['id']] = str(scan['MS:1000512'])
+        run.info["file_object"].close()
         return h_sids
 
     def peaklist(self, scan_id, function_noise="median"):
@@ -64,7 +53,8 @@ class Mzml:
         if function_noise not in ["mean", "median", "mad"]:
             raise ValueError("select a function that is available [mean, median, mad]")
 
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             if scan["id"] == scan_id:
                 peaks = scan.peaks("raw")
                 if len(peaks) > 0:
@@ -81,7 +71,6 @@ class Mzml:
                 header = scan['MS:1000512']
                 mz_range = mz_range_from_header(header)
                 ms_level = scan['ms level']
-
                 pl = PeakList(ID=scan["id"], mz=mzs, intensity=ints,
                               mz_range=mz_range,
                               header=header,
@@ -92,6 +81,7 @@ class Mzml:
                               function_noise=function_noise)
                 snr = np.divide(ints, scan.estimated_noise_level(mode=function_noise))
                 pl.add_attribute('snr', snr)
+                run.info["file_object"].close()
                 return pl
         return None
 
@@ -99,22 +89,25 @@ class Mzml:
 
         if function_noise not in ["mean", "median", "mad"]:
             raise ValueError("select a function that is available [mean, median, mad]")
-
-        # somehow i can not access the scans directly when run() uses an open archive object
-        # print self.run()[2] fails ...
-        return [self.peaklist(scan["id"], function_noise) for scan in self.run() if scan["id"] in scan_ids]
+        run = pymzml.run.Reader(self.filename)
+        pls = [self.peaklist(scan["id"], function_noise) for scan in run if scan["id"] in scan_ids]
+        run.info["file_object"].close()
+        return pls
 
     def tics(self):
-        # somehow i can not access the scans directly when run() uses an open archive object
-        # print self.run()[2]
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             if scan["id"] == "TIC":
-                return zip(*scan.peaks("raw"))[1]
+                hit =  zip(*scan.peaks("raw"))[1]
+                run.info["file_object"].close()
+                return hit
+        run.info["file_object"].close()
         return
 
     def injection_times(self):
         injection_times = {}
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             injection_times[scan['id']] = None
             for element in scan.xmlTree:
                 if "MS:1000927" == element.get('accession'):
@@ -122,11 +115,13 @@ class Mzml:
                     break
             if scan['id'] not in injection_times:
                 injection_times[scan['id']] = None
+        run.info["file_object"].close()
         return injection_times
 
     def scan_dependents(self):
         l = []
-        for scan in self.run():
+        run = pymzml.run.Reader(self.filename)
+        for scan in run:
             if type(scan["id"]) == int:
                 scan_id = scan["id"]
                 if "precursors" in list(scan.keys()):
@@ -137,4 +132,10 @@ class Mzml:
                                 spectrum_ref = int(e[1].split("scan=")[1])
                     if spectrum_ref is not None:
                         l.append([spectrum_ref, scan_id])
+        run.info["file_object"].close()
         return l
+
+    def close(self):
+        self.run.info["file_object"].close()
+        
+    
