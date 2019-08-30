9c133b34715e2f6473c2e18eb0458efd66f22e15
RJMW <r.j.weber@bham.ac.uk>
2019-08-30 12:08:04 +0100

    Fix path in ThermoRaw test

--- tests/test_thermo_raw_portal.py
+++ tests/test_thermo_raw_portal.py
@@ -18,7 +18,7 @@ class ThermoRawPortalsTestCase(unittest.TestCase):
 
     def test_thermo_raw_portal(self):
 
-        run = ThermoRaw(to_test_data("raw", "batch04_QC17_rep01_262.raw"))
+        run = ThermoRaw(to_test_data("raw", "batch04_QC17_rep01_262.RAW"))
         self.assertListEqual(list(run.headers().keys()), ['FTMS + p ESI w SIM ms [70.00-170.00]',
                                                           'FTMS + p ESI w SIM ms [140.00-240.00]',
                                                           'FTMS + p ESI w SIM ms [210.00-310.00]',
