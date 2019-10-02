466dd1eab1fc1b73e1d909f39001aaad96818d01
Ralf Weber <RJMW@users.noreply.github.com>
2019-10-02 16:08:02 +0100

    Bump version to 1.4.0  (for final) py2 release (#55)

--- dimspy/__init__.py
+++ dimspy/__init__.py
@@ -1,5 +1,5 @@
 
 __author__ = 'Ralf Weber (r.j.weber@bham.ac.uk), Albert Zhou (j.zhou.3@bham.ac.uk)'
 __credits__ = 'Ralf Weber (r.j.weber@bham.ac.uk), Albert Zhou (j.zhou.3@bham.ac.uk)'
-__version__ = '1.3.0'
+__version__ = '1.4.0'
 __license__ = 'GPLv3'
